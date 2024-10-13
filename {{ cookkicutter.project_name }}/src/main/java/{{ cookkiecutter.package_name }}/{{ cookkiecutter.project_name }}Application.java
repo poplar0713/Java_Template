4f38bfcc2338aca7c98f23629da9cd5a0086e875
Jeongdong <duwjdehd0713@gmail.com>
2024-10-13 23:07:04 +0900

    create initial template

---
+++ {{ cookkicutter.project_name }}/src/main/java/{{ cookkiecutter.package_name }}/{{ cookkiecutter.project_name }}Application.java
@@ -0,0 +1,13 @@
+package com.ssongman.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class {{ cookkiecutter.project_name }}Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run({{ cookkiecutter.project_name }}Application.class, args);
+	}
+
+}
